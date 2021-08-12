fc8ee9522e9a42caa242b06e1992512805668ba9
Neel Redkar <neel.redkar@outlook.com>
2021-08-11 21:59:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.shortcut("delete_me", async ({ ack, body, say }) => {
 app.shortcut("reply_impression", async ({ ack, body, say }) => {
   await ack();
 
-  if (body.channel.id !== "C02A6BRM2JD" && !["U01LY8S46US", "UJYDFQ2QL", "U0161JDSHGR"].includes(body.user.id)) {
+  if (body.channel.id !== "C02A6BRM2JD" && !["U01LY8S46US", "UJYDFQ2QL", "U0161JDSHGR", "U01VDAQB99P"].includes(body.user.id)) {
     await app.client.chat.postEphemeral({
       token: process.env.SLACK_BOT_TOKEN,
       channel: body.channel.id,
